23deb35b9aeb85af763bba06bc6889c7d3a4188f
javau9hn <147757425+javau9hn@users.noreply.github.com>
2023-11-22 00:54:18 -0500

    updated_ui.py

--- updated_ui.py
+++ updated_ui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
+from spotipy.oauth2 import SpotifyOAuth
 import webbrowser
 # Custom styling for the buttons
 BUTTON_STYLE = {
