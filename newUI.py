5a6a91b070d6efe24954c9bba63766485120d4b5
trose6 <trose66@live.com>
2023-12-17 21:13:47 -0500

    newUI
    Old UI didn't work so had to make a new one

---
+++ newUI.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import subprocess
+import sys
+
+def run_app_py():
+    try:
+        subprocess.run([sys.executable, "app.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running app.py: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+def run_logic_py():
+    try:
+        subprocess.run([sys.executable, "logic.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while running logic.py: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+def create_button(parent, image_file, command):
+    image = Image.open(image_file)
+    photo = ImageTk.PhotoImage(image)
+    button = tk.Button(parent, image=photo, command=command)
+    button.image = photo  # To prevent image garbage collection
+    return button
+
+def main():
+    root = tk.Tk()
+    root.title("Spotify Web Downloader")
+
+    frame = tk.Frame(root)
+    frame.pack(padx=20, pady=20)
+
+    spotify_icon_button = create_button(frame, "spotify_icon.png", run_app_py)
+    spotify_icon_button.pack(side=tk.LEFT, padx=10)
+
+    mnotes_icon_button = create_button(frame, "mnotes.jpeg", run_logic_py)
+    mnotes_icon_button.pack(side=tk.RIGHT, padx=10)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
